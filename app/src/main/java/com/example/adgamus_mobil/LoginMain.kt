2174a501e7b5a69365167282df44546182397649
GVIM0 <guimo.enterprise@gmail.com>
2023-10-28 01:41:47 -0600

    Inicio terminado - No tocar

--- app/src/main/java/com/example/adgamus_mobil/LoginMain.kt
+++ app/src/main/java/com/example/adgamus_mobil/LoginMain.kt
@@ -4,17 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class LoginMain : AppCompatActivity() {
 
+    lateinit var Olvidar: TextView
     lateinit var Login: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_main)
+        setContentView(R.layout.login_main)
 
         // Busca el boton comenzar del inicio
+        Olvidar = findViewById(R.id.Olvidar_contraseña)
         Login = findViewById(R.id.Btn_Login)
 
+        Olvidar.setOnClickListener {
+            val intento = Intent(this, RegisterMain::class.java)
+            startActivity(intento)
+        }
 
         Login.setOnClickListener {
             val intento = Intent(this, Menu_Principal::class.java)
--- app/src/main/java/com/example/adgamus_mobil/RegisterMain.kt
+++ app/src/main/java/com/example/adgamus_mobil/RegisterMain.kt
@@ -1,11 +1,25 @@
 package com.example.adgamus_mobil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterMain : AppCompatActivity() {
+
+    lateinit var Registrame: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_main)
+        setContentView(R.layout.register_main)
+
+        // Busca el boton comenzar del inicio
+        Registrame = findViewById(R.id.Btn_Register)
+
+        Registrame.setOnClickListener {
+            val intento = Intent(this, LoginMain::class.java)
+            startActivity(intento)
+        }
+
     }
 }
