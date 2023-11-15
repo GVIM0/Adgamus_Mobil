0af24280373ed2b7410aa95cec8c04a521042616
GVIM0 <guimo.enterprise@gmail.com>
2023-11-15 00:20:34 -0600

    Registro listo

--- app/src/main/java/com/example/adgamus_mobil/LoginMain.kt
+++ app/src/main/java/com/example/adgamus_mobil/LoginMain.kt
@@ -6,7 +6,14 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class LoginMain : AppCompatActivity() {
 
@@ -14,8 +21,10 @@ class LoginMain : AppCompatActivity() {
     lateinit var Olvidar: TextView
     lateinit var Login: Button
 
-    // DB
-    private lateinit var sharedPreferences: SharedPreferences
+    // EditText
+    lateinit var Email: EditText
+    lateinit var Contraseña: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_main)
@@ -24,19 +33,32 @@ class LoginMain : AppCompatActivity() {
         Olvidar = findViewById(R.id.Olvidar_contraseña)
         Login = findViewById(R.id.Btn_Login)
 
+        // EditText
+        Email = findViewById(R.id.Email_edit_log)
+        Contraseña = findViewById(R.id.Contraseña_edit_log)
+
+        // Cambio a register
         Olvidar.setOnClickListener {
             val intento = Intent(this, RegisterMain::class.java)
             startActivity(intento)
         }
 
+        // Inicio de sesion
         Login.setOnClickListener {
-            val intento = Intent(this, Menu_Principal::class.java)
-            startActivity(intento)
+
+            // DB
+            if(EmailValido(Email.text.toString()) && Contraseña.text.isNotBlank()){
+
+            } else {
+                Toast.makeText(this, "Llena todos los campos", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
-        // DB
-        sharedPreferences = getSharedPreferences("user_preferences", Context.MODE_PRIVATE)
+    }
 
-
+    private fun EmailValido(email: String): Boolean {
+        val emailRegex = Regex("^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})")
+        return emailRegex.matches(email)
     }
 }
--- app/src/main/java/com/example/adgamus_mobil/RegisterMain.kt
+++ app/src/main/java/com/example/adgamus_mobil/RegisterMain.kt
@@ -23,16 +23,10 @@ class RegisterMain : AppCompatActivity() {
     lateinit var Contraseña: EditText
     lateinit var Conf_Contraseña: EditText
 
-    // DB
-    private lateinit var sharedPreferences: SharedPreferences
-
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_main)
-
-        // DB
-        sharedPreferences = getSharedPreferences("user_preferences", Context.MODE_PRIVATE)
 
         // EditText
         Email = findViewById(R.id.Email_edit_log)
@@ -47,41 +41,30 @@ class RegisterMain : AppCompatActivity() {
 
             // Datos DB
             if(EmailValido(Email.text.toString()) && Nombre.text.isNotBlank() && Contraseña.text.isNotBlank() && Conf_Contraseña.text.isNotBlank()) {
+
                 if (Contraseña.text.toString() == Conf_Contraseña.text.toString()) {
-                    val url = "http://192.168.1.117/Proyecto_Corluss/insertar.php"
+
+                    // URL Base de datos insercion
+                    val url = "http://172.24.208.1/Adgamus_Movil/Insertar.php"
+
                     val queue = Volley.newRequestQueue(this)
-                    val resultadoPost = object: StringRequest(Request.Method.POST, url,
-                        Response.Listener<String> { response ->
-                            when {
-                                response.contains("Nombre de usuario o correo ya registrado") -> {
-                                    Toast.makeText(this, "Nombre de usuario o correo electrónico ya registrado", Toast.LENGTH_LONG).show()
-                                }
 
-                                response.contains("El usuario se registró de forma exitosa") -> {
-                                    saveUserData(
-                                        Email.text.toString(),
-                                        Nombre.text.toString(),
-                                        Conf_Contraseña.text.toString()
-                                    )
+                    val resultadoPost = object: StringRequest(Request.Method.POST, url, Response.Listener<String> { response ->
 
-                                    // Cambio de actividad
-                                    val intento = Intent(this, LoginMain::class.java)
-                                    startActivity(intento)
-                                }
+                        Toast.makeText(this, "Usuario registrado exitosamente", Toast.LENGTH_LONG).show()
 
-                                else -> {
-                                    Toast.makeText(this, "Error: $response", Toast.LENGTH_LONG).show()
-                                }
-                            }
+                        // Cambio de actividad
+                        val intento = Intent(this, LoginMain::class.java)
+                        startActivity(intento)
+
                         }, Response.ErrorListener { error ->
-                            Toast.makeText(this, "Error en la conexión: $error", Toast.LENGTH_LONG)
-                                .show()
-                        }
-                    ) {
-                        override fun getParams(): MutableMap<String, String>? {
+                            Toast.makeText(this, "Error en la conexión: $error", Toast.LENGTH_LONG).show()
+                        }) {
+                        override fun getParams(): MutableMap<String, String> {
                             val parametros = HashMap<String, String>()
-                            parametros.put("Correo", Email.text.toString())
-                            parametros.put("Nombre", Nombre.text.toString())
+                            parametros.put("CorreoUsuario", Email.text.toString())
+                            parametros.put("NombreUsuario", Nombre.text.toString())
+                            parametros.put("Contraseña", Conf_Contraseña.text.toString())
                             return parametros
                         }
                     }
@@ -89,24 +72,16 @@ class RegisterMain : AppCompatActivity() {
                 } else {
                     Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
                 }
+
             } else {
                 Toast.makeText(this, "Llena todos los campos", Toast.LENGTH_SHORT).show()
             }
         }
 
     }
-
     private fun EmailValido(email: String): Boolean {
         val emailRegex = Regex("^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})")
         return emailRegex.matches(email)
-    }
-
-    private fun saveUserData(Email: String, Nombre: String, Contraseña: String) {
-        val editor = sharedPreferences.edit()
-        editor.putString("CorreoUsuario", Email)
-        editor.putString("NombreUsuario", Nombre)
-        editor.putString("Contraseña", Contraseña)
-        editor.apply()
     }
 
 }
