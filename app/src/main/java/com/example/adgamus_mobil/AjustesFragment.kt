45c8adc97b635e5d03958638b66ce50d0a7f0d5a
AbsorbedSun <aldoamb.garcia074@gmail.com>
2023-11-19 13:32:58 -0600

    Ajustes_correccion

--- app/src/main/java/com/example/adgamus_mobil/AjustesFragment.kt
+++ app/src/main/java/com/example/adgamus_mobil/AjustesFragment.kt
@@ -5,14 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.adgamus_mobil.databinding.MenuPrincipalBinding
+
 class AjustesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ajustes, container, false)
+        closeBottom()
+    }
+
+    private fun closeBottom(){
+        lateinit var binding: MenuPrincipalBinding
+        binding = MenuPrincipalBinding.inflate(layoutInflater)
+
+        binding.bottomNavigation.background = null
     }
 
 }
